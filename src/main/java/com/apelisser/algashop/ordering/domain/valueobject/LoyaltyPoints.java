0f86ff72060507d066e44a00f464ff63ab8ef57f
Abner J Pelisser <abner.pelisser@gmail.com>
2025-09-02 00:21:42 -0300

    refactor: 5.24 add value objects to the customer

---
+++ src/main/java/com/apelisser/algashop/ordering/domain/valueobject/LoyaltyPoints.java
@@ -0,0 +1,42 @@
+package com.apelisser.algashop.ordering.domain.valueobject;
+
+import java.util.Objects;
+
+public record LoyaltyPoints(Integer value) implements Comparable<LoyaltyPoints> {
+
+    public LoyaltyPoints {
+        Objects.requireNonNull(value);
+
+        if (value < 0) {
+            throw new IllegalArgumentException("Loyalty points cannot be negative");
+        }
+    }
+
+    public LoyaltyPoints() {
+        this(0);
+    }
+
+    public LoyaltyPoints add(LoyaltyPoints other) {
+        Objects.requireNonNull(other);
+        if (other.value() <= 0) {
+            throw new IllegalArgumentException("Loyalty points cannot be negative or zero");
+        }
+
+        return new LoyaltyPoints(this.value() + other.value());
+    }
+
+    public LoyaltyPoints add(Integer value) {
+        return add(new LoyaltyPoints(value));
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+    @Override
+    public int compareTo(LoyaltyPoints o) {
+        return this.value().compareTo(o.value());
+    }
+
+}
---
+++ src/test/java/com/apelisser/algashop/ordering/domain/valueobject/LoyaltyPointsTest.java
@@ -0,0 +1,33 @@
+package com.apelisser.algashop.ordering.domain.valueobject;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LoyaltyPointsTest {
+
+    @Test
+    void shouldGenerate() {
+        LoyaltyPoints loyaltyPoints = new LoyaltyPoints(10);
+        Assertions.assertThat(loyaltyPoints.value()).isEqualTo(10);
+    }
+
+    @Test
+    void shouldAddValue() {
+        LoyaltyPoints loyaltyPoints = new LoyaltyPoints(10);
+        Assertions.assertThat(loyaltyPoints.add(5).value()).isEqualTo(15);
+    }
+
+    @Test
+    void shouldNotAddValue() {
+        LoyaltyPoints loyaltyPoints = new LoyaltyPoints(10);
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> loyaltyPoints.add(-5));
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> loyaltyPoints.add(0));
+
+        Assertions.assertThatExceptionOfType(NullPointerException.class)
+            .isThrownBy(() -> loyaltyPoints.add((LoyaltyPoints) null));
+    }
+
+}
