f87e346f2b927a53a2984fa563351ce2d1ba66f3
Abner J Pelisser <abner.pelisser@gmail.com>
2025-09-20 17:24:58 -0300

    chore: 6.19 control status change

---
+++ src/test/java/com/apelisser/algashop/ordering/domain/entity/OrderStatusTest.java
@@ -0,0 +1,20 @@
+package com.apelisser.algashop.ordering.domain.entity;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrderStatusTest {
+
+    @Test
+    void canChangeTo() {
+        Assertions.assertThat(OrderStatus.DRAFT.canChangeTo(OrderStatus.PLACED)).isTrue();
+        Assertions.assertThat(OrderStatus.DRAFT.canChangeTo(OrderStatus.CANCELED)).isTrue();
+        Assertions.assertThat(OrderStatus.PAID.canChangeTo(OrderStatus.DRAFT)).isFalse();
+    }
+
+    @Test
+    void cannotChangeTo() {
+        Assertions.assertThat(OrderStatus.PLACED.cannotChangeTo(OrderStatus.DRAFT)).isTrue();
+    }
+
+}
