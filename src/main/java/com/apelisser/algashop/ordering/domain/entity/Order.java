f876c25dcdf206c6568bc3a5778bf6acedc07d5b
Abner J Pelisser <abner.pelisser@gmail.com>
2025-09-21 16:07:58 -0300

    chore: 6.24 improve exceptions with factory method

--- src/main/java/com/apelisser/algashop/ordering/domain/entity/Order.java
+++ src/main/java/com/apelisser/algashop/ordering/domain/entity/Order.java
@@ -100,19 +100,9 @@ public class Order {
     }
 
     public void place() {
-        Objects.requireNonNull(this.shipping());
-        Objects.requireNonNull(this.billing());
-        Objects.requireNonNull(this.expectedDeliveryDate());
-        Objects.requireNonNull(this.shippingCost());
-        Objects.requireNonNull(this.paymentMethod());
-        Objects.requireNonNull(this.items());
-
-        if (items().isEmpty()) {
-            throw new OrderCannotBePlacedException(this.id());
-        }
-
-        this.setPlacedAt(OffsetDateTime.now());
+        this.verifyIfCanChangeToPlaced();
         this.changeStatus(OrderStatus.PLACED);
+        this.setPlacedAt(OffsetDateTime.now());
     }
 
     public void markAsPaid() {
@@ -241,6 +231,27 @@ public class Order {
             throw new OrderStatusCannotBeChangedException(this.id(), this.status(), newStatus);
         }
         this.setStatus(newStatus);
+    }
+
+    private void verifyIfCanChangeToPlaced() {
+        if (this.shipping() == null) {
+            throw OrderCannotBePlacedException.noShippingInfo(this.id());
+        }
+        if (this.billing() == null) {
+            throw OrderCannotBePlacedException.noBillingInfo(this.id());
+        }
+        if (this.paymentMethod() == null) {
+            throw OrderCannotBePlacedException.noPaymentMethod(this.id());
+        }
+        if (this.shippingCost() == null) {
+            throw OrderCannotBePlacedException.invalidShippingCost(this.id());
+        }
+        if (this.expectedDeliveryDate() == null) {
+            throw OrderCannotBePlacedException.invalidExpectedDeliveryDate(this.id());
+        }
+        if (this.items() == null || items.isEmpty()) {
+            throw OrderCannotBePlacedException.noItems(this.id());
+        }
     }
 
     private void setId(OrderId id) {
--- src/main/java/com/apelisser/algashop/ordering/domain/exception/ErrorMessages.java
+++ src/main/java/com/apelisser/algashop/ordering/domain/exception/ErrorMessages.java
@@ -13,7 +13,13 @@ public final class ErrorMessages {
     public static final String ERROR_CUSTOMER_ARCHIVED = "Customer is archived it can't be changed";
     public static final String ERROR_ORDER_STATUS_CANNOT_BE_CHANGED = "Cannot change order %s status from %s to %s";
     public static final String ERROR_ORDER_DELIVERY_DATE_CANNOT_IN_THE_PAST = "Order %s expected delivery date cannot be in the past";
-    public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NOT_ITEMS = "Order %s cannot be placed has not items";
+    public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_ITEMS = "Order %s cannot be placed has not items";
+    public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_SHIPPING_INFO = "Order %s cannot be placed, it has no shipping info";
+    public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_BILLING_INFO = "Order %s cannot be placed, it has no billing info";
+    public static final String ERROR_ORDER_CANNOT_BE_PLACED_INVALID_SHIPPING_COST = "Order %s cannot be placed, it has no shipping cost";
+    public static final String ERROR_ORDER_CANNOT_BE_PLACED_INVALID_EXPECTED_DELIVERY_DATE = "Order %s cannot be placed, it has no expected delivery date";
+    public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_PAYMENT_METHOD = "Order %s cannot be placed, it has no payment method";
+
 
     private ErrorMessages() {
         throw new UnsupportedOperationException("Utility class");
