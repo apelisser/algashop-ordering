310593df3c2c6fcdc58b6d7d70644a8275a704ab
Abner J Pelisser <abner.pelisser@gmail.com>
2025-10-08 23:15:34 -0300

    chore: 8.8 isolate integration test execution

---
+++ src/test/java/com/apelisser/algashop/ordering/infrastructure/persistence/entity/OrderPersistenceEntityIT.java
@@ -0,0 +1,14 @@
+package com.apelisser.algashop.ordering.infrastructure.persistence.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPersistenceEntityIT {
+
+    @Test
+    void shouldRun() {
+
+    }
+
+}
