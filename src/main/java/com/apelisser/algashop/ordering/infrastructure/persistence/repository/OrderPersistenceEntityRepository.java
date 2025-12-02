d459e5e31a13dec6121a26473e0baafe69b51914
Abner J Pelisser <abner.pelisser@gmail.com>
2025-12-01 22:41:41 -0300

    chore: 8.34 create queries with jpql

--- src/main/java/com/apelisser/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepository.java
+++ src/main/java/com/apelisser/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepository.java
@@ -2,13 +2,18 @@ package com.apelisser.algashop.ordering.infrastructure.persistence.repository;
 
 import com.apelisser.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface OrderPersistenceEntityRepository extends JpaRepository<OrderPersistenceEntity, Long> {
 
-    List<OrderPersistenceEntity> findByCustomer_IdAndPlacedAtBetween(UUID customerId, OffsetDateTime start, OffsetDateTime end);
+    @Query("""
+        SELECT o FROM OrderPersistenceEntity o
+        WHERE o.customer.id = :customerId
+        AND YEAR(o.placedAt) = :year
+    """)
+    List<OrderPersistenceEntity> placedByCustomerInYear(UUID customerId, Integer year);
 
 }
