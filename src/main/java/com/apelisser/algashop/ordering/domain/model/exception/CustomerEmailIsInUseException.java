ae43ac0a6b70e702c715c8ca7ed966a8e6d9a085
Abner J Pelisser <abner.pelisser@gmail.com>
2026-01-10 16:52:33 -0300

    feat: 9.4 develop the domain service for customer registration

---
+++ src/main/java/com/apelisser/algashop/ordering/domain/model/exception/CustomerEmailIsInUseException.java
@@ -0,0 +1,10 @@
+package com.apelisser.algashop.ordering.domain.model.exception;
+
+import java.io.Serial;
+
+public class CustomerEmailIsInUseException extends DomainException {
+
+    @Serial
+    private static final long serialVersionUID = 8657558650929014106L;
+
+}
