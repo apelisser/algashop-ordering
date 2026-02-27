0172972b97c54bc721bb0b40f6a454ae28922aeb
Abner J Pelisser <abner.pelisser@gmail.com>
2026-02-26 22:43:08 -0300

    chore: 3.6 - add and test location header when creating a customer

--- src/main/java/com/apelisser/algashop/ordering/presentation/CustomerController.java
+++ src/main/java/com/apelisser/algashop/ordering/presentation/CustomerController.java
@@ -6,14 +6,18 @@ import com.apelisser.algashop.ordering.application.customer.query.CustomerFilter
 import com.apelisser.algashop.ordering.application.customer.query.CustomerOutput;
 import com.apelisser.algashop.ordering.application.customer.query.CustomerQueryService;
 import com.apelisser.algashop.ordering.application.customer.query.CustomerSummaryOutput;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.UUID;
 
@@ -32,14 +36,24 @@ public class CustomerController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CustomerOutput create(@RequestBody @Valid CustomerInput input) {
+    public CustomerOutput create(@RequestBody @Valid CustomerInput input, HttpServletResponse response) {
         UUID customerId = customerManagementApplicationService.create(input);
+
+        UriComponentsBuilder builder = MvcUriComponentsBuilder.fromMethodCall(
+            MvcUriComponentsBuilder.on(CustomerController.class).findById(customerId));
+        response.addHeader("Location", builder.toUriString());
+
         return customerQueryService.findById(customerId);
     }
 
     @GetMapping
     public PageModel<CustomerSummaryOutput> findAll(CustomerFilter customerFilter) {
         return PageModel.of(customerQueryService.filter(customerFilter));
+    }
+
+    @GetMapping("/{customerId}")
+    public CustomerOutput findById(@PathVariable UUID customerId) {
+        return customerQueryService.findById(customerId);
     }
 
 }
