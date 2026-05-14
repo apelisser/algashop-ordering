188fec504a82799340521f54f9410420a292d3dd
apelisser <abner.pelisser@gmail.com>
2026-05-13 23:56:45 -0300

    chore: 5.18 challenge - add wiremock to shipping-cost-service-it

--- src/test/java/com/apelisser/algashop/ordering/domain/model/order/shipping/ShippingCostServiceIT.java
+++ src/test/java/com/apelisser/algashop/ordering/domain/model/order/shipping/ShippingCostServiceIT.java
@@ -1,10 +1,18 @@
 package com.apelisser.algashop.ordering.domain.model.order.shipping;
 
+import com.apelisser.algashop.ordering.domain.model.commons.Money;
 import com.apelisser.algashop.ordering.domain.model.commons.ZipCode;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 class ShippingCostServiceIT {
@@ -15,16 +23,33 @@ class ShippingCostServiceIT {
     @Autowired
     OriginAddressService originAddressService;
 
+    WireMockServer wireMockRapidex;
+
+    @BeforeEach
+    void setUp() {
+        wireMockRapidex = new WireMockServer(WireMockConfiguration.options()
+            .port(8780)
+            .usingFilesUnderClasspath("src/test/resources/wiremock/rapidex")
+            .extensions(new ResponseTemplateTransformer(true)));
+
+        wireMockRapidex.start();
+    }
+
+    @AfterEach
+    void tearDown() {
+        wireMockRapidex.stop();
+    }
+
     @Test
     void shouldCalculate() {
         ZipCode origin = originAddressService.originAddress().zipCode();
         ZipCode destination = new ZipCode("12345");
 
         var request = new ShippingCostService.CalculationRequest(origin, destination);
-        var calculate = shippingCostService.calculate(request);
+        var shippingCalculation = shippingCostService.calculate(request);
 
-        Assertions.assertThat(calculate.cost()).isNotNull();
-        Assertions.assertThat(calculate.expectedDate()).isNotNull();
+        Assertions.assertThat(shippingCalculation.cost()).isEqualTo(new Money("35.00"));
+        Assertions.assertThat(shippingCalculation.expectedDate()).isEqualTo(LocalDate.now().plusDays(7));
     }
 
 }
