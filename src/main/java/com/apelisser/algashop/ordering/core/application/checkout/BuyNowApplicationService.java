9ff94329d4a2e12464dd33e3f14ce44002500fc7
apelisser <abner.pelisser@gmail.com>
2026-06-22 23:22:22 -0300

    Revert "chore: 11.10 - use a pure or pragmatic model? understanding the role of a configurator."
    This reverts commit ceae5206e65fc8e50c59164fcb8f29429f206539.

--- src/main/java/com/apelisser/algashop/ordering/core/application/checkout/BuyNowApplicationService.java
+++ src/main/java/com/apelisser/algashop/ordering/core/application/checkout/BuyNowApplicationService.java
@@ -25,9 +25,12 @@ import com.apelisser.algashop.ordering.core.domain.model.product.ProductNotFound
 import com.apelisser.algashop.ordering.core.ports.in.checkout.BuyNowInput;
 import com.apelisser.algashop.ordering.core.ports.in.checkout.ForBuyingProduct;
 import com.apelisser.algashop.ordering.core.ports.in.order.ShippingInput;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 
+@Service
 public class BuyNowApplicationService implements ForBuyingProduct {
 
     private final BuyNowService buyNowService;
@@ -52,6 +55,7 @@ public class BuyNowApplicationService implements ForBuyingProduct {
         this.billingInputDisassembler = billingInputDisassembler;
     }
 
+    @Transactional
     @Override
     public String buyNow(BuyNowInput input) {
         Objects.requireNonNull(input);
